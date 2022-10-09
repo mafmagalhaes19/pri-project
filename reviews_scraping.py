6d80bad60e9fb64a57287ff94655c10fdbb25291
mafmagalhaes19 <mafi.magalhaes@gmail.com>
2022-10-09 10:54:46 +0100

    Finished cleaning the null values of the data set

--- reviews_scraping.py
+++ reviews_scraping.py
@@ -7,20 +7,14 @@ from selenium.webdriver.common.by import By
 
 path_to_file = "reviews.csv"
 
-pages_to_scrape = 1
+def get_restaurant_reviews(restaurant_id, restaurant_url):
+    # import the webdriver
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get(restaurant_url)
 
-url = "https://www.tripadvisor.com/Restaurant_Review-g188590-d11752080-Reviews-Martine_of_Martine_s_Table-Amsterdam_North_Holland_Province.html"
-
-# import the webdriver
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get(url)
-
-# open the file to save the review
-csvFile = open(path_to_file, 'a', encoding="utf-8")
-csvWriter = csv.writer(csvFile)
-
-# change the value inside the range to save the number of reviews we're going to grab
-for i in range(0, pages_to_scrape):
+    # open the file to save the review
+    csvFile = open(path_to_file, 'a', encoding="utf-8")
+    csvWriter = csv.writer(csvFile)
 
     # give the DOM time to load
     time.sleep(2) 
@@ -35,14 +29,9 @@ for i in range(0, pages_to_scrape):
                 driver.execute_script("arguments[0].click();", button)
         except:
                 pass
-        
-    
-    #driver.find_element_by_xpath(".//div[contains(@data-test-target, 'expand-review')]").click()
-
 
     # Now we'll ask Selenium to look for elements in the page and save them to a variable. First lets define a  container that will hold all the reviews on the page. In a moment we'll parse these and save them:
     container = driver.find_elements(By.XPATH, "//*[@class='reviewSelector']")
-
     
     # Next we'll grab the date of the review:
     dates = driver.find_elements(By.XPATH, "//div[contains(@data-prwidget-name, 'reviews_stay_date_hsx')]")
@@ -52,7 +41,7 @@ for i in range(0, pages_to_scrape):
 
     time.sleep(2) 
     
-   # Now we'll look at the reviews in the container and parse them out
+    # Now we'll look at the reviews in the container and parse them out
     for j in range(len(container)): # A loop defined by the number of reviews
 
         # Grab the title
@@ -64,16 +53,11 @@ for i in range(0, pages_to_scrape):
         #Grab the review
         review = container[j].find_element(By.XPATH, ".//p[@class='partial_entry']").text.replace("\n", "  ")
 
-
         #Grab the data
         date = " ".join(dates[j].text.split(" ")[-2:])
-
         
         #Save that data in the csv and then continue to process the next review
-        csvWriter.writerow([date, rating, title, review]) 
-        
-    # When all the reviews in the container have been processed, change the page and repeat            
-    #driver.find_element_by_xpath('.//a[@class="ui_button nav next primary "]').click()
+        csvWriter.writerow([restaurant_id, date, rating, title, review]) 
 
-# When all pages have been processed, quit the driver
-driver.quit()
+    # When all pages have been processed, quit the driver
+    driver.quit()
