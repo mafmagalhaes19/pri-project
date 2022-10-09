293df3c92a9ef0e927780a0952e8b5a7ff5a2243
mafmagalhaes19 <mafi.magalhaes@gmail.com>
2022-10-09 01:32:25 +0100

    Scraper fixed

--- reviews_scraping.py
+++ reviews_scraping.py
@@ -29,25 +29,25 @@ for i in range(0, pages_to_scrape):
     buttons = driver.find_elements(By.XPATH, "//*[@class='reviewSelector']/div/div[2]/div[2]/div/p/span[2]")
 
     for k in range (len(buttons)):
-        time.sleep(2) 
-        button = buttons[k]
-        time.sleep(2) 
-        driver.execute_script("arguments[0].click();", button)
+        try:
+                time.sleep(2) 
+                button = buttons[k]
+                driver.execute_script("arguments[0].click();", button)
+        except:
+                pass
+        
     
     #driver.find_element_by_xpath(".//div[contains(@data-test-target, 'expand-review')]").click()
 
 
     # Now we'll ask Selenium to look for elements in the page and save them to a variable. First lets define a  container that will hold all the reviews on the page. In a moment we'll parse these and save them:
-    time.sleep(2) 
     container = driver.find_elements(By.XPATH, "//*[@class='reviewSelector']")
 
     
     # Next we'll grab the date of the review:
-    time.sleep(2) 
     dates = driver.find_elements(By.XPATH, "//div[contains(@data-prwidget-name, 'reviews_stay_date_hsx')]")
 
     # Grab the titles of all fo the reviews from the current page
-    time.sleep(2) 
     titles = driver.find_elements(By.XPATH, "//*[@class='noQuotes']")
 
     time.sleep(2) 
@@ -56,25 +56,20 @@ for i in range(0, pages_to_scrape):
     for j in range(len(container)): # A loop defined by the number of reviews
 
         # Grab the title
-        time.sleep(2) 
         title = titles[j].text
 
         # Grab the rating
-        time.sleep(2) 
         rating = container[j].find_element(By.XPATH, ".//span[contains(@class, 'ui_bubble_rating bubble_')]").get_attribute("class").split(" ")[1].split("_")[1]
 
         #Grab the review
-        time.sleep(2) 
         review = container[j].find_element(By.XPATH, ".//p[@class='partial_entry']").text.replace("\n", "  ")
 
 
         #Grab the data
-        time.sleep(2) 
         date = " ".join(dates[j].text.split(" ")[-2:])
 
         
         #Save that data in the csv and then continue to process the next review
-        time.sleep(2) 
         csvWriter.writerow([date, rating, title, review]) 
         
     # When all the reviews in the container have been processed, change the page and repeat            
