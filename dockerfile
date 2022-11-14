fa3019fef294ff0779832519fbf6faa6a8faf185
mafmagalhaes19 <mafi.magalhaes@gmail.com>
2022-11-14 00:49:56 +0000

    fixed Dockerfile

--- dockerfile
+++ dockerfile
@@ -1,9 +1,9 @@
 FROM solr:8.10
 
-COPY restaurants.json /datasets/json/restaurants.json
+COPY /datasets/json/restaurants.json /datasets/json/restaurants.json
 
-COPY restaurants_schema.json /datasets/json/schemas/restaurants_schema.json
+COPY /datasets/json/schemas/restaurants_schema.json /datasets/json/schemas/restaurants_schema.json
 
 COPY startup.sh startup.sh
 
-ENTRYPOINT ["sh", "/scripts/startup.sh"]
+ENTRYPOINT ["sh", "startup.sh"]
--- startup.sh
+++ startup.sh
@@ -2,12 +2,11 @@
 
 precreate-core restaurants
 
-precreate-core cuisine-styles
-
-precreate-core reviews
 
 # Start Solr in background mode so we can use the API to upload the schema
 solr start
+
+sleep 10
 
 # Restaurants
 # Schema definition via API
@@ -17,33 +16,6 @@ curl -X POST -H 'Content-type:application/json' \
 
 # Populate collection
 bin/post -c restaurants /datasets/json/restaurants.json
-
-# Restart in foreground mode so we can access the interface
-solr restart -f
-
-
-# Cuisine Styles
-# Schema definition via API
-curl -X POST -H 'Content-type:application/json' \
-    --data-binary @/datasets/json/schemas/cuisine_styles_schema.json \
-    http://localhost:8983/solr/cuisine-styles/schema
-
-# Populate collection
-bin/post -c cuisine-styles /datasets/json/cuisine_styles.json
-
-# Restart in foreground mode so we can access the interface
-solr restart -f
-
-
-# Reviews
-# Schema definition via API
-curl -X POST -H 'Content-type:application/json' \
-    --data-binary @/datasets/json/schemas/reviews_schema.json \
-    http://localhost:8983/solr/reviews/schema
-
-# Populate collection
-bin/post -c reviews /datasets/json/reviews.json
-
 
 # Restart in foreground mode so we can access the interface
 solr restart -f
